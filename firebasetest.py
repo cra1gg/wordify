9ef901c2b4327eeb56edd7a79f9abb8253c0c07d
Craig D'Souza <craigg.dsouza@mail.utoronto.ca>
2020-06-21 16:58:47 -0400

    Update database schema

--- firebasetest.py
+++ firebasetest.py
@@ -2,10 +2,10 @@ from firebase import firebase
 
 firebase = firebase.FirebaseApplication("https://wordify-4eccd.firebaseio.com/", None)
 data = {
-    'Name': 'Parwiz Forogh',
-    'Email': 'par@gmail.com',
-    'Phone': '44707904449'
+    'word': 'testing',
+    'length': 5,
+    'startswith': 't'
 }
 
-result = firebase.post('testing/james', data)
+result = firebase.post('words/', data)
 print(result)
